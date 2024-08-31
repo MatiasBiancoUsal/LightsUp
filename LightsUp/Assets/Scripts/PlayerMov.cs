d8e31dd4b6af07ab33f9aa79a46f3fa035f6a431
iguastavino <i.guastavino@usal.edu.ar>
2024-08-30 23:03:06 -0300

    Logica agacharse
    Implemente la lógica básica para que el jugador pueda agacharse y que su collider se baje. También cambie los nombres de las variables a ingles.

--- LightsUp/Assets/Scripts/PlayerMov.cs
+++ LightsUp/Assets/Scripts/PlayerMov.cs
@@ -4,40 +4,73 @@ using UnityEngine;
 
 public class PlayerMov : MonoBehaviour
 {
-    private float velocidad;
-    public float velocidadCaminar;
-    public float velocidadCorrer;
+    public static PlayerMov Instance;
+
+    private float speed;
+    public float speedWalk;
+    public float speedRun;
     private Rigidbody2D rb2d;
+    private Animator animator;
     public float Horizontal;
+
+    public bool isRunning, isCrouched;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        velocidad = velocidadCaminar;
+        
+        speed = speedWalk;
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Caminar
         Horizontal = Input.GetAxis("Horizontal");
 
         if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
 
         else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        Crouch();
     }
 
     private void FixedUpdate()
     {
+        //Correr
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            velocidad = velocidadCorrer;
+            speed = speedRun;
         } else
         {
-            velocidad = velocidadCaminar;
+            speed = speedWalk;
         }
 
-        rb2d.velocity = new Vector2(Horizontal * velocidad, rb2d.velocity.y);
+        rb2d.velocity = new Vector2(Horizontal * speed, rb2d.velocity.y);
     }
 
+    //Crouch
+    public void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            isCrouched = true;
+            Debug.Log("Anda");
+            animator.SetBool("Crouched", isCrouched);
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            isCrouched = false;
+            animator.SetBool("Crouched", isCrouched);
+        }
+    }
 }
