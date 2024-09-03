1913d1146153213c255657799eff3f4f42b7b39b
laravalentinuz <laramicaela.valentinuz@usal.edu.ar>
2024-09-03 16:14:16 -0300

    Mecánica Push/Pull
    El player ahora puede empujar objetos hacia adelante o arrastrarlos hacia atrás manteniendo presionada la tecla E, y luego moviéndose hacia izq o der

---
+++ LightsUp/Assets/Scripts/PlayerPushPull.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPushPull : MonoBehaviour
+{
+    public float rayDistance = 5f;
+    private GameObject pulledObject;
+    private bool isPulling = false;
+    public LayerMask Pushable;
+
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, rayDistance, Pushable);
+
+        if (Input.GetKeyDown(KeyCode.E) && hit.collider != null)
+        {
+            if (pulledObject == null)
+            {
+                pulledObject = hit.collider.gameObject;
+                isPulling = true;
+            }
+            else if (pulledObject == hit.collider.gameObject)
+            {
+                StopDragging();
+            }
+        }
+
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            isPulling = false;
+        }
+
+        if (isPulling && pulledObject != null)
+        {
+            Drag();
+        }
+    }
+
+    void Drag()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if (pulledObject != null)
+        {
+            Rigidbody2D rb = pulledObject.GetComponent<Rigidbody2D>();
+            rb.velocity = moveDirection * 3f;
+        }
+    }
+
+    void StopDragging()
+    {
+        if (pulledObject != null)
+        {
+            Rigidbody2D rb = pulledObject.GetComponent<Rigidbody2D>();
+            rb.velocity = Vector3.zero;
+            pulledObject = null;
+            isPulling = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.right * transform.localScale.x * rayDistance);
+    }
+}
