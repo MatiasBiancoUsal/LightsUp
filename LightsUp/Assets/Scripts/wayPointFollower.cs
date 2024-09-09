a41d1240689319ab413325882a8e789dad1033b2
mguadafg <mariaguadalupe.fernandezgarcia@usal.edu.ar>
2024-09-09 10:55:29 -0300

    Plataforma movible
    le puse un codigo para q no se deslize el personaje en la plataforma, y q la plataforma  siga los emptys

---
+++ LightsUp/Assets/Scripts/wayPointFollower.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wayPointFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    private int CurrentwaypointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+    private void Update()
+
+    {
+        if (Vector2.Distance(waypoints[CurrentwaypointIndex].transform.position, transform.position) < .1f)
+        {
+            CurrentwaypointIndex++;
+            if (CurrentwaypointIndex >= waypoints.Length)
+            {
+                CurrentwaypointIndex = 0;
+            }
+
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[CurrentwaypointIndex].transform.position, Time.deltaTime * speed);
+    }
+
+
+
+}
