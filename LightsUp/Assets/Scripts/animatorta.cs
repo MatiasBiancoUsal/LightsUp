c9cecf353b15d02535417515405e5230a024f8aa
mguadafg <mariaguadalupe.fernandezgarcia@usal.edu.ar>
2024-09-12 18:04:34 -0300

    prefab personaje (animaciones)
    con chili hicimos la vinculacion de las animaciones a los movimientos del personaje: caminar, idle, correr y saltar (en animator y scripts)
    
    pd: denos el credito q merecemos :´)

---
+++ LightsUp/Assets/Scripts/animatorta.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
+
+public class animatorta : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerMov playerMov; // Reference to the PlayerMov script
+    private PlayerJump playerJump; // Reference to the PlayerJump script
+
+    void Start()
+    {
+        // Get references to the animator, PlayerMov, and PlayerJump components
+        animator = GetComponent<Animator>();
+        playerMov = PlayerMov.instance;
+        playerJump = PlayerJump.Instance;
+    }
+
+    void Update()
+    {
+        // Update animator parameters based on the player movement script
+
+        // Set the running animation
+        animator.SetBool("isRunning", playerMov.isRunning);
+
+        // Set the idle animation when the player is not moving
+        if (playerMov.Horizontal == 0 && playerJump.GroundDetect())
+        {
+            animator.SetBool("isIdle", true);
+        }
+        else
+        {
+            animator.SetBool("isIdle", false);
+        }
+
+        // Transition to walking/running when the player is moving
+        if (playerMov.Horizontal != 0 && !playerMov.isRunning)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        // Set the jumping animation
+        if (!playerJump.GroundDetect())
+        {
+            animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
+        }
+    }
+}
